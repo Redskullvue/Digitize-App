d7bdb73c875862c67eea077fe19eae99fb621e64
redskull422 <kofifa.pg@gmail.com>
2023-02-05 11:51:20 +0330

    Feature : Product Card Component Added

---
+++ src/components/product-card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-white rounded-lg overflow-hidden px-2 py-4 shadow-md">
+    <div class="flex justify-center items-center">
+      <img
+        class="mb-2 rounded-lg"
+        src="../assets/images/watch.jpeg"
+        alt="Image"
+      />
+    </div>
+    <div>
+      <div class="font-light text-gray-200 mt-2 text-xs mb-4 md:text-base">
+        اپل
+      </div>
+      <p class="font-bold text-xs md:text-base">ساعت هوشمند اپل سری 3</p>
+      <p class="font-bold text-sm text-orange-600 text-left mt-3 md:text-base">
+        3,320,000 تومان
+      </p>
+    </div>
+    <div
+      class="text-orange-400 px-2 py-1 mt-4 text-center border-t-2 border-gray-100"
+    >
+      <button class="text-sm font-bold md:text-lg">مشاهده و سفارش</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
