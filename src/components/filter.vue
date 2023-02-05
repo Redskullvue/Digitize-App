025bffe03e21c9c6335eda7fa98e851b52656258
redskull422 <kofifa.pg@gmail.com>
2023-02-05 11:51:46 +0330

    Feature : Added Mobile Filter Bar

---
+++ src/components/filter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="flex items-center justify-between gap-x-4 mx-4 mb-8 bg-gray-100 md:hidden"
+  >
+    <div class="bg-white p-4 rounded flex items-center flex-1">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6 stroke-orange-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm8.25 5.25a.75.75 0 01.75-.75h8.25a.75.75 0 010 1.5H12a.75.75 0 01-.75-.75z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      <span class="text-sm font-medium text-slate-800 mr-2">محبوب ترین</span>
+    </div>
+    <div class="bg-white p-4 rounded flex items-center flex-1">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 stroke-gray-300"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
+          />
+        </svg>
+      </div>
+      <span class="text-sm font-medium text-slate-800 mr-2">فیلتر : اپل</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterSort",
+};
+</script>
+
+<style></style>
