93cf9bb97db7f21f4eb76f8686828f7c468f77b4
redskull422 <kofifa.pg@gmail.com>
2023-02-05 11:50:29 +0330

    Feature : Main Container Added to the project

---
+++ src/components/container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <search-bar />
+    </div>
+    <div class="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-4">
+      <div v-for="n in 10" :key="n">
+        <product-card />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./Search-bar.vue";
+import ProductCard from "./product-card.vue";
+
+export default {
+  name: "YContainer",
+  components: {
+    SearchBar,
+    ProductCard,
+  },
+};
+</script>
+
+<style></style>
