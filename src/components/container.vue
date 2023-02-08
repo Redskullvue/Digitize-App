bd48a1544ff37cf099b2b8c75a1ab3fa0166cbbf
redskull422 <kofifa.pg@gmail.com>
2023-02-08 10:43:06 +0330

    Feature : Desktop Mode Grid Developed

--- src/components/container.vue
+++ src/components/container.vue
@@ -3,15 +3,32 @@
     <div>
       <search-bar />
     </div>
-    <div class="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mx-4">
-      <div v-for="product in productsInfo" :key="product.id">
-        <product-card
-          :name="product.name"
-          :price="product.price"
-          :colors="product.colors"
-          :image="product.image"
-          :brand="product.brand"
-        />
+    <div
+      class="grid grid-cols-12 md:grid-rows-[55px_minmax(500px,_1fr)] md:gap-8"
+    >
+      <!-- Side Bar  -->
+      <div class="bg-white p-4 col-span-3 row-span-2 hidden md:block">
+        Side Bar
+      </div>
+      <!-- Sort Section -->
+      <div class="bg-white p-4 col-span-9 row-span-1 hidden md:block">
+        Sort Section
+      </div>
+      <!-- Product Section -->
+      <div class="col-span-12 md:col-span-9">
+        <div
+          class="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 md:gap-4 mx-4 md:m-0"
+        >
+          <div v-for="product in productsInfo" :key="product.id">
+            <product-card
+              :name="product.name"
+              :price="product.price"
+              :colors="product.colors"
+              :image="product.image"
+              :brand="product.brand"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
