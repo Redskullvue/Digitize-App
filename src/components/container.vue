a4f6664d774636a30aef16174c097d91227a3fbf
redskull422 <kofifa.pg@gmail.com>
2023-02-10 16:16:02 +0330

    Feature :  SideBar Section Added

--- src/components/container.vue
+++ src/components/container.vue
@@ -7,12 +7,75 @@
       class="grid grid-cols-12 md:grid-rows-[55px_minmax(500px,_1fr)] md:gap-8"
     >
       <!-- Side Bar  -->
-      <div class="bg-white p-4 col-span-3 row-span-2 hidden md:block">
-        Side Bar
+      <div class="col-span-3 row-span-2 hidden md:block">
+        <side-bar />
       </div>
       <!-- Sort Section -->
-      <div class="bg-white p-4 col-span-9 row-span-1 hidden md:block">
-        Sort Section
+      <div class="col-span-9 row-span-1 hidden md:block">
+        <div
+          class="bg-white px-4 flex items-center gap-x-4 text-gray-400 rounded-lg"
+        >
+          <div class="bg-orange-200 rounded-md p-2 text-orange-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentcolor"
+              class="w-7 h-7"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+              />
+            </svg>
+          </div>
+
+          <button @click="sorting.sort = 1" class="py-4 relative">
+            <span v-if="sorting.sort !== 1">محبوب ترین</span>
+            <span v-if="sorting.sort == 1" class="font-bold text-slate-800"
+              >محبوب ترین</span
+            >
+            <span
+              v-if="sorting.sort == 1"
+              class="w-2 h-2 rounded-lg absolute bg-orange-600 top-1 left-0"
+            ></span>
+          </button>
+
+          <button @click="sorting.sort = 2" class="py-4 relative">
+            <span v-if="sorting.sort != 2"> پر بازدید ترین</span>
+            <span v-if="sorting.sort == 2" class="font-bold text-slate-800">
+              پر بازدید ترین</span
+            >
+            <span
+              v-if="sorting.sort == 2"
+              class="w-2 h-2 rounded-lg absolute bg-orange-600 top-1 left-0"
+            ></span>
+          </button>
+
+          <button @click="sorting.sort = 3" class="py-4 relative">
+            <span v-if="sorting.sort != 3">گران ترین </span>
+            <span v-if="sorting.sort == 3" class="font-bold text-slate-800"
+              >گران ترین
+            </span>
+            <span
+              v-if="sorting.sort == 3"
+              class="w-2 h-2 rounded-lg absolute bg-orange-600 top-1 left-0"
+            ></span>
+          </button>
+
+          <button @click="sorting.sort = 4" class="py-4 relative">
+            <span v-if="sorting.sort != 4"> پر فروش ترین </span>
+            <span v-if="sorting.sort == 4" class="font-bold text-slate-800">
+              پر فروش ترین
+            </span>
+            <span
+              v-if="sorting.sort == 4"
+              class="w-2 h-2 rounded-lg absolute bg-orange-600 top-1 left-0"
+            ></span>
+          </button>
+        </div>
       </div>
       <!-- Product Section -->
       <div class="col-span-12 md:col-span-9">
@@ -37,12 +100,14 @@
 <script>
 import SearchBar from "./Search-bar.vue";
 import ProductCard from "./product-card.vue";
+import sideBar from "./side-bar.vue";
 
 export default {
   name: "YContainer",
   components: {
     SearchBar,
     ProductCard,
+    sideBar,
   },
   data() {
     return {
@@ -87,7 +152,16 @@ export default {
           price: "3,250,000",
           colors: ["bg-red-500", "bg-blue-900", "bg-pink-500"],
         },
+        {
+          id: "5",
+          name: "ساعت هوشمند اپل سری6 ",
+          image: "../assets/images/watch.jpeg",
+          brand: "اپل",
+          price: "10,250,000",
+          colors: ["bg-red-500", "bg-blue-900", "bg-pink-500"],
+        },
       ],
+      sorting: { sort: 1, activeClass: "text-slate-800 font-bold" },
     };
   },
 };
---
+++ src/components/side-bar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="bg-white p-5 rounded-xl">
+    <!-- Catergories -Desktop Mode -->
+    <div>
+      <div class="text-orange-500 text-lg font-bold mb-5">دسته بندی</div>
+      <ul class="mb-7">
+        <li class="mb-1 hover:bg-gray-100 cursor-pointer rounded-md">
+          <a href="#" class="flex items-center py-2">
+            <span class="relative h-6 w-6 rounded-full bg-gray-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-6 absolute -bottom-1 -left-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
+              </svg>
+            </span>
+            <span class="mr-2 text-slate-800">گوشی موبایل</span></a
+          >
+        </li>
+
+        <li class="mb-1 hover:bg-gray-100 cursor-pointer rounded-md">
+          <a href="#" class="flex items-center py-2">
+            <span class="relative h-6 w-6 rounded-full bg-gray-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-6 absolute -bottom-1 -left-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
+                />
+              </svg>
+            </span>
+            <span class="mr-2 text-slate-800">لپ تاپ</span>
+          </a>
+        </li>
+
+        <li class="hover:bg-gray-100 cursor-pointer rounded-md">
+          <a href="#" class="flex items-center py-2">
+            <span class="relative h-6 w-6 rounded-full bg-gray-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-6 absolute -bottom-1 -left-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+            <span class="mr-2 text-slate-800">ساعت هوشمند</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- Filters -Desktop Filter -->
+    <div class="">
+      <div class="text-orange-500 text-lg font-bold mb-5">فیلتر ها</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
