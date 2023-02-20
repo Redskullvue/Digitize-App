9932b627d62ba8776a7369b52d459dab2417e120
redskull422 <pouya.9996@gmail.com>
2023-02-20 13:44:28 +0330

    Feature : Single Product Page Completed

--- src/components/singleproduct.vue
+++ src/components/singleproduct.vue
@@ -78,11 +78,37 @@
           <div
             class="mt-10 flex flex-col items-center mx-4 p-2 md:flex-row md:justify-start md:items-start"
           >
-            <img
-              class="w-64 h-64"
-              :src="require(`@/assets/images/${singleProductInfo.image}.png`)"
-              alt="Product Image"
-            />
+            <div class="flex flex-col justify-center items-center">
+              <img
+                class="w-64 h-64"
+                :src="require(`@/assets/images/${singleProductInfo.image}.png`)"
+                alt="Product Image"
+              />
+              <div class="flex flex-row w-32 h-32 mt-5 justify-center gap-x-2">
+                <img
+                  class="w-14 h-14 ring-1 ring-orange-500 rounded-lg"
+                  :src="
+                    require(`@/assets/images/${singleProductInfo.image}.png`)
+                  "
+                  alt=""
+                />
+                <img
+                  class="w-14 h-14 ring-1 ring-orange-500 rounded-lg"
+                  :src="
+                    require(`@/assets/images/${singleProductInfo.image}.png`)
+                  "
+                  alt=""
+                />
+                <img
+                  class="w-14 h-14 ring-1 ring-orange-500 rounded-lg"
+                  :src="
+                    require(`@/assets/images/${singleProductInfo.image}.png`)
+                  "
+                  alt=""
+                />
+              </div>
+            </div>
+
             <div class="flex flex-col md:mb-2">
               <div class="mt-1 text-center md:text-start">
                 <h2 class="text-slate-800 md:text-xl">
@@ -104,35 +130,104 @@
                   ></div>
                 </div>
               </div>
-            </div>
-          </div>
-
-          <!-- Seller Deatil Section -->
-          <div class="flex flex-wrap gap-x-2 px-2 text-xs md:hidden">
-            <div class="flex items-center justify-center flex-1">
-              <span class="mx-2">
-                <i-shop class="w-4 h-4" />
-              </span>
-              <span class="font-bold">فروشنده :</span>
-              <span class="text-slate-800">دیجی زون</span>
-            </div>
-            <div class="flex items-center justify-center flex-1">
-              <span class="mx-2">
-                <i-grauntee class="w-4 h-5" />
-              </span>
-              <span class="font-bold"> گارانتی :</span>
-              <span class="text-slate-800"> 18 ماه زرین خدمت</span>
-            </div>
-            <div class="flex items-center w-full justify-center">
-              <span class="mx-2">
-                <i-forward class="w-4 h-5" />
-              </span>
-              <span class="font-bold"> ارسال توسط :</span>
-              <span class="text-slate-800">انبار تهران</span>
+              <!-- Seller Deatil Section -->
+              <div class="flex flex-wrap gap-x-2 px-2 text-xs md:hidden">
+                <div class="flex items-center justify-center flex-1">
+                  <span class="mx-2">
+                    <i-shop class="w-4 h-4" />
+                  </span>
+                  <span class="font-bold">فروشنده :</span>
+                  <span class="text-slate-800">دیجی زون</span>
+                </div>
+                <div class="flex items-center flex-1">
+                  <span class="mx-2">
+                    <i-grauntee class="w-4 h-5" />
+                  </span>
+                  <span class="font-bold"> گارانتی :</span>
+                  <span class="text-slate-800"> 18 ماه زرین </span>
+                </div>
+                <div class="flex items-center w-full justify-center">
+                  <span class="mx-2">
+                    <i-forward class="w-4 h-5" />
+                  </span>
+                  <span class="font-bold"> ارسال توسط :</span>
+                  <span class="text-slate-800">انبار تهران</span>
+                </div>
+              </div>
+              <!-- Product Deatil -->
+              <div
+                class="md:flex flex-col text-slate-800 bg-white p-4 mt-4 rounded-lg md:p-0 text-sm md:text-lg"
+              >
+                <h2 class="font-bold mb-4">ویژگی های کالا</h2>
+                <ul class="md:list-disc list-inside marker:text-orange-500">
+                  <li class="flex flex-col gap-y-1 md:flex-row">
+                    <span class="text-gray-500">حافظه داخلی : </span>
+                    <span>256 گیگابایت</span>
+                  </li>
+                  <li class="flex flex-col gap-y-1 md:flex-row">
+                    <span class="text-gray-500"> اندازه صفحه نمایش: </span>
+                    <span>6.0 اینچ و بزرگتر</span>
+                  </li>
+                  <li class="flex flex-col gap-y-1 md:flex-row">
+                    <span class="text-gray-500"> شبکه های ارتباطی : </span>
+                    <span>2G , 3G , 4G , 5G</span>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-        <div class="hidden md:block bg-red-100 p-8">Hello World</div>
+        <div class="hidden text-sm md:block bg-white text-slate-800 w-64">
+          <div class="bg-gray-200 p-4 rounded-lg mt-4 ml-2">
+            <ul class="flex flex-col gap-y-4">
+              <li class="flex items-center">
+                <span class="mx-2">
+                  <i-shop class="w-4 h-4" />
+                </span>
+                <span class="font-bold">فروشنده :</span>
+                <span class="text-slate-600">دیجی زون</span>
+              </li>
+              <li class="flex items-center">
+                <span class="mx-2">
+                  <i-grauntee class="w-4 h-5" />
+                </span>
+                <span class="font-bold"> گارانتی :</span>
+                <span class="text-slate-600"> 18 ماه زرین </span>
+              </li>
+              <li class="flex items-center">
+                <span class="mx-2">
+                  <i-forward class="w-4 h-5" />
+                </span>
+                <span class="font-bold"> ارسال :</span>
+                <span class="text-slate-600">انبار تهران</span>
+              </li>
+              <li class="text-center font-bold w-full mt-8">
+                <span class="text-orange-600 text-lg"
+                  >{{ singleProductInfo.price }} تومان</span
+                >
+                <button
+                  class="bg-orange-500 text-white mt-4 p-3 rounded-lg w-full hover:bg-orange-600"
+                >
+                  افزودن به سبد خرید
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- Mobile Fix Add To Cart Button -->
+        <div class="bg-white fixed bottom-0 w-full p-2 md:hidden">
+          <div class="flex items-center gap-x-4">
+            <button
+              class="bg-orange-500 text-white p-3 rounded-lg w-56 hover:bg-orange-600"
+            >
+              افزودن به سبد خرید
+            </button>
+            <div class="text-gray-800 text-lg text-center">
+              {{ singleProductInfo.price }}
+              <p class="text-gray-500">تومان</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -160,7 +255,6 @@ export default {
   },
   created() {
     this.singleProductInfo = JSON.parse(this.data);
-    console.log(this.singleProductInfo);
     document.title = this.singleProductInfo.name;
   },
 };
